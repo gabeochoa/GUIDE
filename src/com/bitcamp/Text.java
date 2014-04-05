7eb38e9a412350da1031ab8b77cb4f8b80e5caeb
Gabe <gabe1118@yahoo.com>
2014-04-05 06:48:08 -0400

    added text demo

---
+++ src/com/bitcamp/Text.java
@@ -0,0 +1,42 @@
+package com.bitcamp;
+
+import java.awt.BorderLayout;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
+
+import org.fife.ui.rsyntaxtextarea.RSyntaxTextArea;
+import org.fife.ui.rsyntaxtextarea.SyntaxConstants;
+import org.fife.ui.rtextarea.RTextScrollPane;
+
+public class Text extends JFrame {
+
+   public Text() {
+
+      JPanel cp = new JPanel(new BorderLayout());
+
+      RSyntaxTextArea textArea = new RSyntaxTextArea(20, 60);
+      textArea.setSyntaxEditingStyle(SyntaxConstants.SYNTAX_STYLE_JAVA);
+      textArea.setCodeFoldingEnabled(true);
+      RTextScrollPane sp = new RTextScrollPane(textArea);
+      cp.add(sp);
+
+      setContentPane(cp);
+      setTitle("Text Editor Demo");
+      setDefaultCloseOperation(EXIT_ON_CLOSE);
+      pack();
+      setLocationRelativeTo(null);
+
+   }
+
+   public static void main(String[] args) {
+      // Start all Swing applications on the EDT.
+      SwingUtilities.invokeLater(new Runnable() {
+         public void run() {
+            new Text().setVisible(true);
+         }
+      });
+   }
+
+}
