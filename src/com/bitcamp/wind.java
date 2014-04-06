1fb45d6211dfd57389a7b439711ca54541369698
Gabe <gabe1118@yahoo.com>
2014-04-06 07:35:22 -0400

    save, saee as

--- src/com/bitcamp/wind.java
+++ src/com/bitcamp/wind.java
@@ -1,6 +1,5 @@
 package com.bitcamp;
 
-
 import java.awt.BorderLayout;
 import java.awt.Toolkit;
 import java.awt.datatransfer.Clipboard;
@@ -15,6 +14,7 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.io.UnsupportedEncodingException;
 import java.nio.charset.Charset;
 import java.util.ArrayList;
@@ -38,7 +38,7 @@ public class wind extends JFrame implements ActionListener
 	 */
 	private static final long serialVersionUID = 7447036913279756726L;
 	public String direc = "";
-	public String dir = "";
+	public String dir = "nofile";
 	public wind(JDesktopPane sc)
 	{
 		setTitle("guIDE");
@@ -68,6 +68,19 @@ public class wind extends JFrame implements ActionListener
 		menuItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_O,
 				ActionEvent.ALT_MASK));
 		menuItem.setActionCommand("Open");
+		menuItem.addActionListener(this);
+		filemenu.add(menuItem);
+
+		menuItem = new JMenuItem("Save");
+		menuItem.setMnemonic(KeyEvent.VK_S);
+		menuItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_S,
+				ActionEvent.CTRL_MASK));
+		menuItem.setActionCommand("Save");
+		menuItem.addActionListener(this);
+		filemenu.add(menuItem);
+		
+		menuItem = new JMenuItem("Save As");
+		menuItem.setActionCommand("Save-as");
 		menuItem.addActionListener(this);
 		filemenu.add(menuItem);
 
@@ -144,7 +157,7 @@ public class wind extends JFrame implements ActionListener
 	public void actionPerformed(ActionEvent e)
 	{
 		/*
-		 * User wants a refreash
+		 * User wants a refresh
 		 */
 		if("refresh".equals(e.getActionCommand()))
 		{ // new
@@ -154,38 +167,39 @@ public class wind extends JFrame implements ActionListener
 		else if("Open".equals(e.getActionCommand()))
 		{
 			final JFileChooser fc = new JFileChooser();
+			fc.setDialogTitle("Open");
 			int returnVal = fc.showOpenDialog(this);
 			if (returnVal == JFileChooser.APPROVE_OPTION) {
-	            File file = fc.getSelectedFile();
-	            //This is where a real application would open the file.
-	            System.out.println("Opening: " + file.getName() + ".");
-	            
-	    		InputStream    fis = null;
-	    		BufferedReader br;
-	    		String         line;
-	    		try
+				File file = fc.getSelectedFile();
+				//This is where a real application would open the file.
+				System.out.println("Opening: " + file.getName() + ".");
+
+				InputStream    fis = null;
+				BufferedReader br;
+				String         line;
+				try
 				{
-	    			dir = file.getPath();
+					dir = file.getPath();
 					fis = new FileInputStream(file.getPath());
 				} catch (FileNotFoundException e1)
 				{
 					// TODO Auto-generated catch block
 					e1.printStackTrace();
 				}
-	    		br = new BufferedReader(new InputStreamReader(fis, Charset.forName("UTF-8")));
-	    		try
+				br = new BufferedReader(new InputStreamReader(fis, Charset.forName("UTF-8")));
+				try
 				{
 					while ((line = br.readLine()) != null) {
-					    //filetext.add(line);
-					    WorkingDemo.appentText(line);
+						//filetext.add(line);
+						WorkingDemo.appentText(line);
 					}
 				} catch (IOException e1)
 				{
 					// TODO Auto-generated catch block
 					e1.printStackTrace();
 				}
-	    		// Done with the file
-	    		try
+				// Done with the file
+				try
 				{
 					br.close();
 				} catch (IOException e1)
@@ -193,12 +207,64 @@ public class wind extends JFrame implements ActionListener
 					// TODO Auto-generated catch block
 					e1.printStackTrace();
 				}
-	    		br = null;
-	    		fis = null;
-	        } else {
-	        	System.out.println("Open command cancelled by user.");
-	        }
-			
+				br = null;
+				fis = null;
+			} else {
+				System.out.println("Open command cancelled by user.");
+			}
+
+		}
+		else if("Save".equals(e.getActionCommand()))
+		{
+			if(dir.equals("nofile"))
+			{
+				final JFileChooser fc = new JFileChooser();
+				fc.setDialogTitle("Save");
+				int returnVal = fc.showSaveDialog(this);
+				if (returnVal == JFileChooser.APPROVE_OPTION) {
+					File file = fc.getSelectedFile();
+					dir = file.getPath();
+				}
+			}
+			PrintWriter writer = null;
+			try
+			{
+				writer = new PrintWriter(dir, "UTF-8");
+			} catch (FileNotFoundException e1)
+			{
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			} catch (UnsupportedEncodingException e1)
+			{
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+			writer.println(WorkingDemo.getRawCodeTextBlock());
+			writer.close();
+		}
+		else if("Save-as".equals(e.getActionCommand()))
+		{
+			JFileChooser chooser = new JFileChooser();
+			chooser.setDialogTitle("Save As");
+			chooser.setCurrentDirectory(new File(dir));
+			int retrival = chooser.showSaveDialog(null);
+			if (retrival == JFileChooser.APPROVE_OPTION) {
+				PrintWriter writer = null;
+				try
+				{
+					writer = new PrintWriter(chooser.getSelectedFile(), "UTF-8");
+				} catch (FileNotFoundException e1)
+				{
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				} catch (UnsupportedEncodingException e1)
+				{
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+				writer.println(WorkingDemo.getRawCodeTextBlock());
+				writer.close();
+			}
 		}
 		else if("Compile and Run".equals(e.getActionCommand()))
 		{
@@ -251,8 +317,8 @@ public class wind extends JFrame implements ActionListener
 		}
 	}
 
-	
-	
+
+
 	/*
 	 * public void fetchUrl() throws IOException {
 	 * 
@@ -392,24 +458,23 @@ public class wind extends JFrame implements ActionListener
 	{
 
 		ArrayList<String> command = new ArrayList<String>();
-	   	command.add("g++");
-	    command.add("-o");
-	    command.add("avc");
-	    command.add("helloworld.cpp");
-	    SysComEx sce = new SysComEx(command);
-	    		
-	    int result = 0;
+		command.add("g++");
+		command.add("-o");
+		command.add("avc");
+		command.add("helloworld.cpp");
+		SysComEx sce = new SysComEx(command);
+
+		int result = 0;
 		System.out.println("result val: " + result);
 		// get the output from the command
 		StringBuilder stdout = sce.getStandardOutputFromCommand();
 		StringBuilder stderr = sce.getStandardErrorFromCommand();
 
-//		// print the output from the command
+		//		// print the output from the command
 		System.out.println("STDOUT");
 		System.out.println(stdout);
 		System.out.println("STDERR");
 		System.out.println(stderr);
-		
 		WorkingDemo.getRawCodeTextBlock();
 
 		/* Our source file */
@@ -419,13 +484,13 @@ public class wind extends JFrame implements ActionListener
 		/*
 		 * Write the compile command to the terminal.
 		 */
-//		ArrayList<String> command = new ArrayList<String>();
-//		command.add("usr/var/make ");
-//		command.add("g++");
-//		command.add(" -o ");
-//		command.add(sourceName + " ");
-//		command.add(executableName + " ");
-//		command.add(sourceName);
+		//		ArrayList<String> command = new ArrayList<String>();
+		//		command.add("usr/var/make ");
+		//		command.add("g++");
+		//		command.add(" -o ");
+		//		command.add(sourceName + " ");
+		//		command.add(executableName + " ");
+		//		command.add(sourceName);
 		//ProcessBuilder pb = new ProcessBuilder(command);
 		//Process compileDatCode = pb.start();
 	}
